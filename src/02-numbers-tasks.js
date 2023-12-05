e58cc85cf7c571d5a1e307068d8dfac6e81fac16
zilinkijgena0 <152978414+zilinkijgena0@users.noreply.github.com>
2023-12-05 15:02:22 +0300

    Задача 6
    /**
     * Returns an angle (in radians) between two vectors given by xi and yi,
     * coordinates in Cartesian plane.
     * See details https://en.wikipedia.org/wiki/Euclidean_vector#Representations
     *
     * @param {number} x1
     * @param {number} y1
     * @param {number} x2
     * @param {number} y2
     * @return {number}
     *
     * @example:
     *   (1,0) (0,1)     => π/2
     *   (0,1) (0,-1)    => π
     *   (0,-1) (1,0)    => π/2
     *   (0,1) (0,1)     => 0
     *   (0,1) (1,2)     => 0
     */

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -107,8 +107,11 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const p1 = (x1 * x2 + y1 * y2);
+  const p2 = Math.sqrt(x1 * x1 + y1 * y1);
+  const p3 = Math.sqrt(x2 * x2 + y2 * y2);
+  return Math.acos(p1 / (p2 * p3));
 }
 
 /**
