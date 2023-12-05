718713bda42751307b32579f7acb56dae45a60b7
zilinkijgena0 <152978414+zilinkijgena0@users.noreply.github.com>
2023-12-05 14:52:28 +0300

    Задача 5
    /**
     * Returns a first char of the given string.
     *
     * @param {string} value
     * @return {string}
     *
     * @example
     *   'John Doe'  => 'J'
     *   'cat'       => 'c'
     */

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -77,8 +77,8 @@ function extractNameFromTemplate(value) {
  *   'John Doe'  => 'J'
  *   'cat'       => 'c'
  */
-function getFirstChar(/* value */) {
-  throw new Error('Not implemented');
+function getFirstChar(value) {
+  return value[0];
 }
 
 /**
