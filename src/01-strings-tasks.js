c44ceb95f38214e89ecc74952e982357b1a8787b
zilinkijgena0 <152978414+zilinkijgena0@users.noreply.github.com>
2023-12-05 14:50:38 +0300

    Задача 2
    /**
     * Returns the length of given string.
     *
     * @param {string} value
     * @return {number}
     *
     * @example
     *   'aaaaa' => 5
     *   'b'     => 1
     *   ''      => 0
     */

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -32,8 +32,8 @@ function concatenateStrings(value1, value2) {
  *   'b'     => 1
  *   ''      => 0
  */
-function getStringLength(/* value */) {
-  throw new Error('Not implemented');
+function getStringLength(value) {
+  return value.length;
 }
 
 /**
