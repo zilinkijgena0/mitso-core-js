eab19b46bd7d6a933fe77834cd13bab62ab271f8
zilinkijgena0 <152978414+zilinkijgena0@users.noreply.github.com>
2023-12-05 14:54:52 +0300

    Задача 9
    /**
     * Remove the first and last angle brackets from tag string
     *
     * @param {string} str
     * @return {string}
     *
     * @example
     *   '<div>' => 'div'
     *   '<span>' => 'span'
     *   '<a>' => 'a'
     */

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -138,8 +138,8 @@ function removeFirstOccurrences(str, value) {
  *   '<span>' => 'span'
  *   '<a>' => 'a'
  */
-function unbracketTag(/* str */) {
-  throw new Error('Not implemented');
+function unbracketTag(str) {
+  return str.slice(1, str.length - 1);
 }
 
 /**
