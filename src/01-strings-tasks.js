a9744bbe386e5742069d118c28bbcce132b2cbb6
zilinkijgena0 <152978414+zilinkijgena0@users.noreply.github.com>
2023-12-05 14:55:21 +0300

    Задача 10
    /**
     * Converts all characters of the specified string into the upper case
     *
     * @param {string} str
     * @return {string}
     *
     * @example
     *   'Thunderstruck' => 'THUNDERSTRUCK'
     *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     */

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -152,8 +152,8 @@ function unbracketTag(str) {
  *   'Thunderstruck' => 'THUNDERSTRUCK'
  *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
  */
-function convertToUpperCase(/* str */) {
-  throw new Error('Not implemented');
+function convertToUpperCase(str) {
+  return str.toUpperCase();
 }
 
 /**
